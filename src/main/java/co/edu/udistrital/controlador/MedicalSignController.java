5bd06c5433efbcec212a3eaf9f26c5dcb1878eb1
Kevin Gordillo <ksgordillo94@gmail.com>
2019-05-23 22:14:43 -0500

    se agrega transmision al web socket

--- src/main/java/co/edu/udistrital/controlador/MedicalSignController.java
+++ src/main/java/co/edu/udistrital/controlador/MedicalSignController.java
@@ -1,12 +1,11 @@
 package co.edu.udistrital.controlador;
 
-import java.util.Collection;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
 
 import co.edu.udistrital.dominio.MedicalSignal;
 import co.edu.udistrital.websocket.WsClient;
@@ -19,14 +18,11 @@ public class MedicalSignController {
 	
 	@PostMapping("/sendSignal")
 	String capturarSenal(@RequestBody MedicalSignal senalMedica){
-		WsClient.sendBroadcastMessage(senalMedica.getDato());
+		final Gson gson = new Gson();
+		final String senalMedicaJson = gson.toJson(senalMedica);
+		WsClient.sendBroadcastMessage(senalMedicaJson);
 		final String mensajeRetorno = "Recieved signal: "+senalMedica.getDato();
 		return mensajeRetorno;
 	}
-	
-	@GetMapping("/hello")
-    public Collection<String> sayHello() {
-        return null;
-    }
 	
 }
--- src/main/java/co/edu/udistrital/websocket/WsClient.java
+++ src/main/java/co/edu/udistrital/websocket/WsClient.java
@@ -9,51 +9,12 @@ import javax.websocket.WebSocketContainer;
 
 @ClientEndpoint
 public class WsClient {
-//	private static Object waitLock = new Object();
-	// Session userSession = null;
 	
 	private static WebSocketContainer container;
 	private static Session session;
 	public WsClient() {
 
 	}
-
-	// @PostConstruct
-	// public void Init(){
-	// WebSocketContainer container = null;//
-	// Session session = null;
-	// try {
-	// container = ContainerProvider.getWebSocketContainer();
-	// session = container.connectToServer(WsClient.class,
-	// URI.create("ws://localhost:8081/WebSocketServer/endpoint?push=TIME"));
-	// wait4TerminateSignal();
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// } finally {
-	// if (session != null) {
-	// try {
-	// session.close();
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
-	// }
-	// }
-
-//	@OnMessage
-//	public void onMessage(String message) {
-//		// the new USD rate arrives from the websocket server side.
-//		System.out.println("Received msg: " + message);
-//	}
-
-//	private static void wait4TerminateSignal() {
-//		synchronized (waitLock) {
-//			try {
-//				waitLock.wait();
-//			} catch (InterruptedException e) {
-//			}
-//		}
-//	}
 
 	public static void sendBroadcastMessage(final String mensaje) {
 		
@@ -63,7 +24,7 @@ public class WsClient {
 				session = container.connectToServer(WsClient.class,
 						URI.create("ws://localhost:8081/WebSocketServer/endpoint"));
 			}
-			session.getAsyncRemote().sendText("Valor de la señal enviada: " + mensaje);
+			session.getAsyncRemote().sendText(mensaje);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
